3a3fd685cdd43e38f58b4229b97164efc1751b6c
Carlos Espino Timón <carlosespinotimon@gmail.com>
2019-04-23 05:43:17 +0100

    Cleaning code

--- backend/server/decorators.py
+++ backend/server/decorators.py
@@ -8,8 +8,6 @@ from .helpers.db_helper import get_table
 def check_token(func):
     @wraps(func)
     def decorated_funcion(*args, **kwargs):
-        # from google.oauth2 import id_token
-        # from google.auth.transport import requests
         import requests
         try:
             token = request.headers.get('access_token')
@@ -20,16 +18,12 @@ def check_token(func):
                 user_table = get_table('users')
                 user = user_table.select(
                                 user_table.c.email == email).execute().first()
-                # return True if user else False
                 if user:
                     return func(*args, **kwargs)
                 else:    
                     return jsonify('Unauthorized'), 401
-            # return False
             return jsonify('Unauthorized'), 401
         except Exception as e:
-            # Invalid token
-            print("invalid"  , e)
             pass
             return jsonify('Error'), 500
     return decorated_funcion
