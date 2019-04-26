99c183299271846641b8dec37cdafd0a7dd9e908
Carlos Espino Timón <carlosespinotimon@gmail.com>
2019-04-26 05:31:14 +0100

    Create/Update user with photo

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -47,6 +47,22 @@ def create_app(app_config='config.Config'):
     from .auth import auth
     app.register_blueprint(auth.auth)
 
+    # A simple page that says server status
+    @app.route('/image', methods=['POST'])
+    @cross_origin()
+    def image():
+        # print(request.get_json())
+        req = request.get_json()
+        image = req.get('image', None)
+        if image:
+            from .helpers.images import upload_image
+            try:
+                print(upload_image(image, 'image/png', '.psng'))
+            except Exception as e:
+                print(e)
+         
+
+        return jsonify('The server is running!!')
     
 
    
--- backend/server/users/helper.py
+++ backend/server/users/helper.py
@@ -5,6 +5,8 @@ from sqlalchemy import Table, desc
 from server.helpers.db_helper import get_table, get_session
 from server.models.customer import Customer
 
+import requests
+
 
 def get_customer(id):
     '''
@@ -32,7 +34,7 @@ def create_customer(sess, data, user_id):
     stmt = cust_table.insert().values(
         name = data['name'],
         surname = data['surname'],
-        photo_url = data['photo_url'],
+        photo_url = data.get('photo_url'),
         created_by = user_id,
         last_modify_by = user_id
     )
@@ -56,11 +58,14 @@ def update_customer(sess, customer_id, data, user_id):
     stmt = cust_table.update().values(
         name = data['name'],
         surname = data['surname'],
-        photo_url = data['photo_url'],
+        photo_url = data.get('photo_url'),
         last_modify_by = user_id
     ).where(cust_table.c.id == customer_id)
-
-    sess.execute(stmt)
+    
+    res = sess.execute(stmt)
+    if res.rowcount == 0:
+        raise Exception("User not found")
+    
 
 def delete_customer_by_id(sess, customer_id):
     '''
@@ -75,3 +80,41 @@ def delete_customer_by_id(sess, customer_id):
         sess.execute(stmt)
     except Exception:
         print("EEEE")
+
+def get_user_id(token):
+    '''
+    Function that given a token, it returns its user id.
+    :param srt token: the token.
+    :return: the id of the user.
+    :rtype: int
+    '''
+    try:
+        url = 'https://www.googleapis.com/plus/v1/people/me?access_token='
+        res = requests.get(url+token)
+        if res.status_code == 200:
+            email = res.json()['emails'][0]['value']
+            user_table = get_table('users')
+            user = user_table.select(
+                            user_table.c.email == email).execute().first()
+            if user:
+                return user['id']
+        raise Exception('User not found', 500)
+    except Exception as e:
+        pass
+        print("Error: ", e)
+        raise Exception('Server error', 500)
+
+def get_url_stored_image(customer_id):
+    '''
+    Function that given a customer_id returns the url of its image.
+    :param int customer_id: the id of the customer.
+    :return: the url of the image.
+    :rtype: string
+    '''
+    cust_table = get_table('customers')
+    res = cust_table.select(cust_table.c.id == customer_id).execute().first()
+    return res['photo_url'] if res else []
+    
+    
+
+
